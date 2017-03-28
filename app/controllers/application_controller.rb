578360376c3391e016e2ce636b43269bbd77840f
leichaofan <leichaofan2024@gmail.com>
2017-03-28 16:58:50 +0800

    add shopping-cart in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  helper_method :current_cart
+
+  
   def current_cart
     @current_cart ||= find_cart
   end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,6 @@
 class Cart < ApplicationRecord
   has_many :cartitems
+  has_many :product, :through => :cartitems, :source => :product 
 
   def add_product_to_cart(product)
     ci = cartitems.build
