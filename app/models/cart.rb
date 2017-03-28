ddd6dceaf2bcc54d74c3f596883fa772aae7f67d
leichaofan <leichaofan2024@gmail.com>
2017-03-28 18:45:56 +0800

    cart total price

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,11 +1,17 @@
 class Cart < ApplicationRecord
   has_many :cartitems
-  has_many :product, :through => :cartitems, :source => :product 
+  has_many :product, :through => :cartitems, :source => :product
 
   def add_product_to_cart(product)
     ci = cartitems.build
     ci.product = product
     ci.save
   end
-
+  def total_price
+    sum = 0
+    cartitems.each do |cartitem|
+      sum += cartitem.product.price * cartitem.quantity
+    end
+    sum
+  end
 end
