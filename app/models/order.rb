b2212fdf34f4811e9e0eb5a97a09ef7753487eaa
leichaofan <leichaofan2024@gmail.com>
2017-04-06 22:10:06 +0800

    add is_paid and payment_method to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,13 +3,20 @@ class Order < ApplicationRecord
 
   def generate_token
     self.token = SecureRandom.uuid
-  end 
+  end
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
   belongs_to :user
   has_many :product_lists
+
+  def set_payment_with!(method)
+    self.update_columns(:payment_method => method)
+  end
+  def pay!
+    self.update_columns(is_paid: true)
+  end 
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406102056) do
+ActiveRecord::Schema.define(version: 20170406125624) do
 
   create_table "cartitems", force: :cascade do |t|
     t.integer  "product_id"
@@ -41,9 +41,11 @@ ActiveRecord::Schema.define(version: 20170406102056) do
     t.string   "billing_address"
     t.string   "shipping_name"
     t.string   "shipping_address"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.string   "token"
+    t.boolean  "is_paid",          default: false
+    t.string   "payment_method"
   end
 
   create_table "product_lists", force: :cascade do |t|
