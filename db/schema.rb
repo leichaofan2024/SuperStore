e2ff4f5ff2c387b2ad1d50ee17233e5602e6bcfc
leichaofan <leichaofan2024@gmail.com>
2017-04-09 10:17:17 +0800

    add image and nickname to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408151633) do
+ActiveRecord::Schema.define(version: 20170409013512) do
 
   create_table "cartitems", force: :cascade do |t|
     t.integer  "product_id"
@@ -98,6 +98,8 @@ ActiveRecord::Schema.define(version: 20170408151633) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
+    t.string   "nickname"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
