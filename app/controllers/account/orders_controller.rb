32fbba81e90e3bc8e9338eef50d190db27a32e27
leichaofan <leichaofan2024@gmail.com>
2017-04-14 16:20:32 +0800

    beautify account/order/index.html.erb

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -3,6 +3,12 @@ class Account::OrdersController < ApplicationController
 
   def index
     @orders = current_user.orders.order("created_at DESC")
+    if params[:status] == "paid"
+      @orders = @orders.where(:aasm_state => "paid")
+    end
+    if params[:status] == "unpaid"
+      @orders = @orders.where(:aasm_state => "order_placed")
+    end
   end
 
 
