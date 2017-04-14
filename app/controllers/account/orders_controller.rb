51a2bf546fdfb91e0a1b1a257772800c3f475091
leichaofan <leichaofan2024@gmail.com>
2017-04-14 18:47:46 +0800

    account order bug and beautify session page

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -4,10 +4,10 @@ class Account::OrdersController < ApplicationController
   def index
     @orders = current_user.orders.order("created_at DESC")
     if params[:status] == "paid"
-      @orders = @orders.where(:aasm_state => "paid")
+      @orders = @orders.where(:is_paid => true)
     end
     if params[:status] == "unpaid"
-      @orders = @orders.where(:aasm_state => "order_placed")
+      @orders = @orders.where(:is_paid => false)
     end
   end
 
